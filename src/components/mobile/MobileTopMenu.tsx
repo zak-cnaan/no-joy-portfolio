e2c1eb1d5e5aeb6ab917953a782a1dd2e51c5be6
Zak <izak@moonsite.co.il>
2023-12-26 19:34:23 +0200

    drawer

--- src/components/mobile/MobileTopMenu.tsx
+++ src/components/mobile/MobileTopMenu.tsx
@@ -1,6 +1,11 @@
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+
 import styled from "styled-components";
 import TopNav from "../menues/TopNav";
 import { responsive } from "../../assets/styles/bp";
+import { FaBars } from "react-icons/fa6";
+import { useState } from 'react';
 
 const Div = styled.div`
   ${responsive.min_md} {
@@ -8,13 +13,28 @@ const Div = styled.div`
   }
 `;
 
-function MobileTopMenu() {
-  return (
-    <Div>
-      <button>Hamborger</button>
-      <TopNav />
-    </Div>
-  );
+
+const D = styled(Drawer)`
+  background-color: hsl(var(--color-muted)) !important;
+`
+const MobileTopMenu = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+
+    return (
+        <Div>
+            <button onClick={toggleDrawer}><FaBars /></button>
+            <D
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='left'
+            >
+                <TopNav />
+            </D>
+        </Div>
+    )
 }
 
 export default MobileTopMenu;
